4560b770b78d6b60320a249e739931a682bc3e01
nkonko <nicoazzara@hotmail.com>
2018-10-24 00:00:08 -0300

    prueba exect

---
+++ DAL/Utils/BaseDao.cs
@@ -0,0 +1,30 @@
+﻿namespace DAL.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using Dapper;
+
+    public class BaseDao
+    {
+        public void Exec(string query)
+        {
+            using (IDbConnection connection = SqlUtils.Connection())
+            {
+                connection.Open();
+                connection.Execute(query);
+            }
+        }
+
+        public T Exec<T>(string query)
+        {
+            using (IDbConnection connection = SqlUtils.Connection())
+            {
+                connection.Open();
+                var result = (List<T>)connection.Query<T>(query);
+
+                return (T)Convert.ChangeType(result, typeof(T));
+            }
+        }
+    }
+}
